6a6796a6a679d0a5786e8f2bc8b74222f12008e8
Marcos <marcossanchezbajo@gmail.com>
2020-08-23 22:26:55 +0200

    Added code to connect to db and retrieve rows from logs table. The table and database are created manually.

--- version.c
+++ version.c
@@ -1,10 +1,9 @@
+#include <mysql/mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mysql/mysql.h>
 
-int main(int argc, char **argv)
-{
-  printf("MySQL client version: %s\n", mysql_get_client_info());
+int main(int argc, char **argv) {
+    printf("MySQL client version: %s\n", mysql_get_client_info());
 
-  exit(0);
+    exit(0);
 }
