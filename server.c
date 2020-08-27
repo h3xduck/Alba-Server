3b7c991eb813a99e7e221b9500bdd23e49b77ebf
Marcos <marcossanchezbajo@gmail.com>
2020-08-27 18:45:29 +0200

    Now the server successfully splits the JSON into parts, and waits for instructions afterwards (instructions not implemented yet)

--- server.c
+++ server.c
@@ -1,48 +1,47 @@
+#include <json-c/json.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <time.h>
-#include "database.h"
-#include <json-c/json.h>
-#include <signal.h>
-#include <pthread.h>
+#include <unistd.h>
 
-void signalHandler(){
+#include "database.h"
+
+void signalHandler() {
     printf("Process exited by signal handler\n");
     exit(0);
 }
 
-json_object* create_custom_json(struct resultStringArray resultArray){
-    json_object * jobj = json_object_new_object();
-    json_object *jstring0 = json_object_new_string(resultArray.contentArray[0]);
-    json_object *jstring1 = json_object_new_string(resultArray.contentArray[1]);
-    json_object *jstring2 = json_object_new_string(resultArray.contentArray[2]);
+json_object* create_custom_json(struct resultStringArray resultArray) {
+    json_object* jobj = json_object_new_object();
+    json_object* jstring0 = json_object_new_string(resultArray.contentArray[0]);
+    json_object* jstring1 = json_object_new_string(resultArray.contentArray[1]);
+    json_object* jstring2 = json_object_new_string(resultArray.contentArray[2]);
 
-    json_object_object_add(jobj,"Date", jstring0);
-    json_object_object_add(jobj,"Content", jstring1);
-    json_object_object_add(jobj,"User", jstring2);
-    printf ("The json object created: %s\n",json_object_to_json_string(jobj));
-    
+    json_object_object_add(jobj, "Date", jstring0);
+    json_object_object_add(jobj, "Content", jstring1);
+    json_object_object_add(jobj, "User", jstring2);
+    printf("The json object created: %s\n", json_object_to_json_string(jobj));
+
     return jobj;
 }
 
-void* reading_thread_routine(void* params){
-    int sock = *((int*) params);
+void* reading_thread_routine(void* params) {
+    int sock = *((int*)params);
     printf("Reader thread received %i as parameter\n");
-    
-    
-    char * line = NULL;
+
+    char* line = NULL;
     size_t len = 0;
     ssize_t read;
     FILE* fp = fdopen(sock, "r");
-    if(fp == NULL){
+    if (fp == NULL) {
         perror("Couldn't open file pointer");
     }
-    while(0){
-        
+    while (0) {
         while ((read = getline(&line, &len, fp)) != -1) {
             printf("Retrieved line of length %zu:\n", read);
             printf("Message received from client: %s\n", line);
@@ -50,23 +49,20 @@ void* reading_thread_routine(void* params){
         sleep(5);
         printf("Attempted to read...\n");
     }
-    
 
     printf("Finished reader thread\n");
-
 }
 
-void send_init_connection_message(int sock){
-    int fillerArrayLength = 1024*sizeof(char);
-    char *fillerArray = malloc(fillerArrayLength);
+void send_init_connection_message(int sock) {
+    int fillerArrayLength = 1024 * sizeof(char);
+    char* fillerArray = malloc(fillerArrayLength);
     memset(fillerArray, 0, fillerArrayLength);
-    printf("Filler array is: %s", fillerArray);
     printf("Called init connection message\n");
 
     char* header = "STARTCONN::";
     char* separator = "\n##ALBA##\n";
-    int totalLength = strlen(header)+ strlen(separator)+ fillerArrayLength+ 1;
-    char* to_send = calloc(strlen(header)+ strlen(separator)+ fillerArrayLength+ 1, sizeof(char));
+    int totalLength = strlen(header) + strlen(separator) + fillerArrayLength + 1;
+    char* to_send = calloc(strlen(header) + strlen(separator) + fillerArrayLength + 1, sizeof(char));
     strcpy(to_send, header);
     strcat(to_send, separator);
     strcat(to_send, fillerArray);
@@ -82,10 +78,35 @@ void send_init_connection_message(int sock){
     free(to_send);
 }
 
+void send_end_connection_message(int sock) {
+    int fillerArrayLength = 1024 * sizeof(char);
+    char* fillerArray = malloc(fillerArrayLength);
+    memset(fillerArray, 0, fillerArrayLength);
+    printf("Called end connection message\n");
+
+    char* header = "ENDCONN::";
+    char* separator = "\n##ALBA##\n";
+    int totalLength = strlen(header) + strlen(separator) + fillerArrayLength + 1;
+    char* to_send = calloc(strlen(header) + strlen(separator) + fillerArrayLength + 1, sizeof(char));
+    strcpy(to_send, header);
+    strcat(to_send, separator);
+    strcat(to_send, fillerArray);
+    printf("Total length is %i\n", totalLength);
+
+    int n = write(sock, to_send, 1024);
+    printf("Sending ENDCONN message\n");
+    if (n < 0) {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
+    free(fillerArray);
+    free(to_send);
+}
+
 void doprocessing(int sock) {
-    pthread_t reading_thread; //We need another thread to process the data sent by the client.
+    pthread_t reading_thread;  //We need another thread to process the data sent by the client.
     int n;
-    char *fillerArray = malloc(1024);
+    char* fillerArray = malloc(1024);
     bzero(fillerArray, 1024);
     //pthread_create(&reading_thread, NULL, reading_thread_routine, &sock);
 
@@ -94,63 +115,63 @@ void doprocessing(int sock) {
     connectDB();
     struct resultStringArray result = getLastRow();
 
+    send_init_connection_message(sock);  //Announcing start of message to the client
+
     json_object* jobj = create_custom_json(result);
-    char* jobjstr = (char*)json_object_to_json_string_ext(jobj,JSON_C_TO_STRING_PRETTY);
+    char* jobjstr = (char*)json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_PRETTY);
     printf("Received JSON string was: %s\n", jobjstr);
     char* header = "INCLUDE::";
     char* delimitor = "\n##ALBA##\n";
-    char* to_send = calloc(strlen(jobjstr)+strlen(header)+strlen(delimitor)+ sizeof(fillerArray)+1, sizeof(char));
-    
+    char* to_send = calloc(strlen(jobjstr) + strlen(header) + strlen(delimitor) + sizeof(fillerArray) + 1, sizeof(char));
+
     //Now that we have the complete message, we proceed to adjust it to the protocol.
     char* token = strtok(jobjstr, "\n");
-    printf("Part 0 is: %s\n", token);
+    int ii = 0;
+    while (token != NULL) {
+        printf("Part %i is: %s\n", ii, token);
 
-    //First, the header
-    strcpy(to_send, header);
-    //Next, the content of the message (the token in this case)
-    strcat(to_send, token);
-    //We put a delimitor, unlikely to appear in a normal connection
-    strcat(to_send, delimitor);
-    //Finally we concatenate the string with the filler string
-    strcat(to_send, fillerArray);
-    
-    
+        //First, the header
+        strcpy(to_send, header);
+        //Next, the content of the message (the token in this case)
+        strcat(to_send, token);
+        //We put a delimitor, unlikely to appear in a normal connection
+        strcat(to_send, delimitor);
+        //Finally we concatenate the string with the filler string
+        strcat(to_send, fillerArray);
 
-    send_init_connection_message(sock);
+        printf("Sending \"%s\" to the client\n", to_send);
+        printf("Writing %i bytes on socket stream\n", 1024);
+        n = write(sock, to_send, 1024);
+        if (n < 0) {
+            perror("ERROR writing to socket");
+            exit(1);
+        }
 
-    
-
-    printf("Sending \"%s\" to the client\n", to_send);
-    printf("Writing %i bytes on socket stream\n", 1024);
-    n = write(sock, to_send, 1024);
-    printf("Completed!\n");
-    if (n < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
+        //Next token
+        token = strtok(NULL, "\n");
+        ii++;
     }
+    printf("Completed message in %i rounds!\n", ii);
+
+    send_end_connection_message(sock);  //Announcing end of message to the client
+
 
     //printf("Flushed buffer to client\n");
-    while (0){
+    while (1) {
         printf("Trapped\n");
         sleep(5);
-        //TODO: IF YOU SET THIS TO 1, CLIENT RECEIVES NOTHING.
         //Trapped here for now, until implementation of listening of client's commands.
         //Must not exit the proccess.
     }
-    
 }
 
-
-
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char* argv[]) {
     int sockfd, newsockfd, portno, clilen;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
     int n, pid;
     int conn_number = 0;
 
-    
     /* First call to socket() function */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -160,7 +181,7 @@ int main(int argc, char *argv[]) {
     }
 
     /* Initialize socket structure */
-    bzero((char *)&serv_addr, sizeof(serv_addr));
+    bzero((char*)&serv_addr, sizeof(serv_addr));
     portno = 5001;
 
     serv_addr.sin_family = AF_INET;
@@ -168,15 +189,13 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_port = htons(portno);
 
     /* Now bind the host address using bind() call.*/
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("ERROR on binding");
         exit(1);
     }
 
-
     //Registering SIGINT signal handler. The signal is received by BOTH THE PARENT AND CHILD.
-    signal(SIGINT, signalHandler); 
-
+    signal(SIGINT, signalHandler);
 
     printf("Started listening to connections\n");
     /* Now start listening for the clients, here
@@ -188,7 +207,7 @@ int main(int argc, char *argv[]) {
     clilen = sizeof(cli_addr);
 
     while (1) {
-        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &clilen);
         printf("Accepting client connection...\n");
         if (newsockfd < 0) {
             perror("ERROR on accept");
